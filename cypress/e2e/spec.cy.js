60d08fd3e07457b9f89ee09d978f3a0b66f9d4c7
danrojas393-art <danrojas393@gmail.com>
2026-05-06 14:44:07 -0600

    feat: primera prueba de login en saucedemo con cypress

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,12 @@
+describe('prueba en saucedemo', () => {
+it('puede iniciar secion',() => {
+  cy.visit('https://www.saucedemo.com/')
+  cy.get('#user-name').type('standard_user')
+  cy.get('#password').type('secret_sauce')
+  cy.get('#login-button').click()
+  cy.contains('Products').should('be.visible')
+ })
+})
+
+
+ 
